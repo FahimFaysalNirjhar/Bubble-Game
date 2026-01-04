ce73e1bf8d4d8354fb201d76ed780312541a0ca2
Fahim Faysal Nirjhar <fahimfaysal1995@gmail.com>
2026-01-05 00:28:26 +0600

    score function completed

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const palateBottom = document.querySelector(".palate-bottom");
 const timerValue = document.getElementById("timer");
+const scoreValue = document.getElementById("score");
 
 let hitNum = 0;
 let score = 0;
@@ -7,8 +8,8 @@ let score = 0;
 const createBubble = () => {
   let container = "";
   for (let i = 0; i <= 206; i++) {
-    hitNum = Math.floor(Math.random() * 10);
-    container += `<div class="round">${hitNum}</div>`;
+    let num = Math.floor(Math.random() * 10);
+    container += `<div class="round">${num}</div>`;
   }
   palateBottom.innerHTML = container;
 };
@@ -31,5 +32,12 @@ timer();
 
 const updateScore = () => {
   score += 10;
+  scoreValue.textContent = score;
 };
-updateScore();
+palateBottom.addEventListener("click", (event) => {
+  console.log(event.target.textContent);
+  let number = Number(event.target.textContent);
+  if (hitNum === number) {
+    updateScore();
+  }
+});
